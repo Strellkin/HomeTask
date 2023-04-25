4dafa669e5f56fda562eaf5c27ca8498fd69cec9
Sergey <sergey.leffchenko@gmail.com>
2023-04-25 12:54:52 +0200

    feat: add new task 6 and 4 with its solution

--- Program.cs
+++ Program.cs
@@ -22,9 +22,50 @@ if (number_1 < number_2)
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
+/*
+Console.Write ("Input your first number: ");
+int number_1 = Convert.ToInt32(Console.ReadLine ());
+Console.Write ("Input your second number: ");
+int number_2 = Convert.ToInt32(Console.ReadLine ());
+Console.Write ("Input your third number: ");
+int number_3 = Convert.ToInt32(Console.ReadLine ());
+int max = number_1;
+
+if (number_2 > max)
+{ 
+    max = number_2;
+} 
+if (number_3 > max)
+{
+    max = number_3;
+}
+Console.Write ($"Your maximum number is  {max}");
+*/
 
 
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+// 1. Ввод числа
+// 2. Проверить является ли число чётным?
+// 3. Вывести соответствующее сообщение пользователю
 
-//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*
+Console.Write ("Input your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number % 2 == 0)
+{
+    Console.WriteLine ("Your number is even");
+}
+else
+{
+    Console.WriteLine ("Your number is odd");
+}
+*/
+
 
 //Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+
